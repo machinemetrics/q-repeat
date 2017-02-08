4eae91585f584c15cd92041a1ea90b0785a68223
Justin Aquadro <justin.aquadro@machinemetrics.com>
2017-02-08 21:20:17 +0000

    Add a timeout function that will keep repeating body until it succeeds within timeout

--- test/q-repeat.test.js
+++ test/q-repeat.test.js
@@ -1,3 +1,4 @@
+'use strict';
 /* eslint-env mocha */
 
 const expect = require('chai').expect;
@@ -11,6 +12,31 @@ describe('map', () => {
       return Q.map([1, 2, 3], x => x * x * x);
     }).then(results => {
       expect(results).to.eql([1, 8, 27]);
+    });
+  });
+});
+
+describe('timeoutRepeat', () => {
+  it('repeats until success', () => {
+    let delay = 500;
+    let invocations = 0;
+
+    return Q.timeoutRepeat(150, () => {
+      delay -= 100;
+      invocations += 1;
+      return Q(`answer ${invocations}`).delay(delay);
+    }).then((result) => {
+      expect(delay).to.eq(100);
+      expect(invocations).to.eq(4);
+      expect(result).to.eq('answer 4');
+    });
+  });
+
+  it('fails fast if actual error occurs', () => {
+    return Q.timeoutRepeat(500, () => {
+      throw new Error('omg');
+    }).catch((err) => {
+      expect(err.message).to.eq('omg');
     });
   });
 });
